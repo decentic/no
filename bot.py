f40c5ec35c619bd97cb02d5b56c6ca5f9b0b998d
decentic <52168433+decentic@users.noreply.github.com>
2019-07-08 16:49:08 +0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,51 @@
+import discord
+
+import youtube_dl
+
+from discord.ext import commands
+
+TOKEN = 'NTkzMzY1MDI4MzE3NTYwODQz.XRy-Dw.ocrd8mmU3tDZMKbzcaR6rYjrstU'
+
+client = commands.Bot(command_prefix = '$')
+
+players = {}
+
+@client.event
+
+async def on_ready():
+
+         print('Bot online')
+
+@client.command(pass_context=True)
+
+async def join(ctx):
+
+         channel = ctx.message.author.voice.voice_chanel
+
+await client.join_voice_channel(channel)
+
+@client.command(pass_context=True)
+
+async def leave(ctx):
+
+server = ctx.message.server
+
+voice_client = client.voice_client_in(server)
+
+await voice_client.disconnect()
+
+@client.command(pass_context=True)
+
+async def play(ctx, url):
+
+server = ctx.message.server
+
+voice_client =  client.voice_client_in(server)
+
+player = await voice_client.create_ytdl_player(url)
+
+players[server.id] = player
+
+player.start()
+
+client.run(TOKEN)
